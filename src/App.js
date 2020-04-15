6a26606dff5c0482d18d5753e549e2caf8cf4140
Scott Plomer <scplomer27@gmail.com>
2020-04-14 23:29:05 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
 import Axios from 'axios';
+//https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02 
+class App extends React.Component {
 
-class App extends React.Component() {
+  state = {
+    apiCode : '201401e464692be4b503a36ae395369e',
+    googleApi: 'AIzaSyCCbSlc0isJHzODIpWRvDncBDbEMsILT-s'
+  }
 
-  componentdid
+componentDidMount(){
+  Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${this.state.apiCode}&units=imperial`)
+  .then(res => res.data)
+  .then(data => console.log(data))
+  .catch (err => console.log(err))
+}
 
   render(){
     return (
       <div className="App">
-        test
+        <input type ='text' placeholder ='Enter Location'/>
       </div>
     );
   }
