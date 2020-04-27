f1de47756233086113f8f04e1315d1d4c392586d
Scott Plomer <scplomer27@gmail.com>
2020-04-26 19:00:27 -0500

    Updates

---
+++ precache-manifest.a0dd3685c3af57b770f8aad011a9b3f9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2aadd1ed86debd1a159341d6b26e1e",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "1802eba18085b56e38a4",
+    "url": "/React-Weather-App/static/css/main.9cf1c467.chunk.css"
+  },
+  {
+    "revision": "9ce32323edf5f3dcec88",
+    "url": "/React-Weather-App/static/js/2.230365ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Weather-App/static/js/2.230365ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1802eba18085b56e38a4",
+    "url": "/React-Weather-App/static/js/main.2f3877ed.chunk.js"
+  },
+  {
+    "revision": "1ca072ff12dbb5fc3ee3",
+    "url": "/React-Weather-App/static/js/runtime-main.cbff98cc.js"
+  },
+  {
+    "revision": "d402e76fffae476bc75a1e68d8be8862",
+    "url": "/React-Weather-App/static/media/OtherSky.d402e76f.jpg"
+  },
+  {
+    "revision": "8be3f6d2f160d1b12b3e0e4e4b474778",
+    "url": "/React-Weather-App/static/media/clearSky.8be3f6d2.jpg"
+  },
+  {
+    "revision": "855ce5a77c9e0385a683d2e0211b2819",
+    "url": "/React-Weather-App/static/media/cloudSky.855ce5a7.jpg"
+  },
+  {
+    "revision": "eba111d12040220b2a1974807a48d96d",
+    "url": "/React-Weather-App/static/media/rainSky.eba111d1.jpg"
+  }
+]);
